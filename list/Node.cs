401cc13364e9018b0d41a4796e7113dcbb62a20a
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-21 16:25:24 -0800

    Add files via upload

---
+++ list/Node.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linkedlist
+{
+  public  class Node
+    {
+        
+        
+            public int data; //actual data stored in the node
+            public Node next;// pointer in next node
+            public Node(int d)
+            {
+            //initializing the data and pointer 
+                data = d;
+                next = null;
+            }
+        public void print()
+        {
+            Console.Write("|" + data + "|");
+            if (next != null)
+            {
+                next.print();
+            }
+        }
+        public void insertsorted(int data)
+        {
+            if(next == null)
+            {
+                next = new Node(data);
+            }
+            else if (data < next.data)
+            {
+                Node temp = new Node(data);
+                temp.next = this.next;
+                this.next = temp;
+            }
+            else
+            {
+                next.insertsorted(data);
+            }
+        }
+        public void addnode(int data)
+        {
+            if(next == null)
+            {
+                next = new Node(data);
+            }
+            else
+            {
+                next.addnode(data);
+            }
+        }
+    }
+
+    public class mlist
+    {
+        public Node headnode;
+
+        public mlist()
+        {
+            headnode = null;
+        }
+        /* public void addtoend(int data)
+         {
+             if(headnode == null)
+             {
+                 headnode = new Node(data);
+             }
+             else
+             {
+                 headnode.addtoend(data);
+             }
+         }*/
+        public void addnode(int data)
+        {
+            if (headnode == null)
+            {
+                headnode = new Node(data);
+            }
+            else
+            {
+                headnode.addnode(data);
+            }
+        }
+        public void insertsorted(int data)
+        {
+            if (headnode == null)
+            {
+                headnode = new Node(data);
+            }
+            else if (data < headnode.data)
+            {
+                insertbeginnig(data);
+            }
+            else
+            {
+                headnode.insertsorted(data);
+            }
+        }
+        public void insertbeginnig(int data)
+        {
+            if (headnode == null)
+            {
+                headnode = new Node(data);
+            }
+            else
+            {
+                Node temp = new Node(data);
+                temp.next = headnode;
+                headnode = temp;
+            }
+        }
+        public void print()
+        {
+            if(headnode != null)
+            {
+                headnode.print();
+            }
+        }
+    }
+}
---
+++ list/slinkedlist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linkedlist
+{
+    class slinkedlist
+    {
+        private Node head; //starting node
+
+        public slinkedlist()
+        {
+            head = null;
+
+        }
+        public void DisplayList()
+        {
+            Node i;
+            if(head == null)
+            {
+                Console.WriteLine("empty list");
+                return;
+            }
+
+        }
+    }
+}
