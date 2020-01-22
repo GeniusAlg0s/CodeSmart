fbb880c8c64bd1135eeb6a08f1c7b8beec825b39
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-21 16:46:17 -0800

    Add files via upload

---
+++ arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace arrayfun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            Console.WriteLine("length: " + numbers.Length);
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9 : " + index);
+
+            Array.Clear(numbers, 0, 1); //range of elements
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            var test = new int[3];
+            Array.Copy(numbers, test, 3);
+
+            Console.WriteLine("copy");
+            foreach (var n in test)
+                Console.WriteLine( n);
+            Console.WriteLine("\n");
+
+            Array.Sort(numbers);
+            Array.Clear(numbers, 0, 1); //range of elements
+            foreach (var n in numbers)
+                Console.WriteLine(  n);
+            Console.WriteLine("\n");
+
+            Array.Reverse(numbers);
+           
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+            Console.WriteLine("\n");
+        }
+    }
+}
