1978e0adbb9a9e4f78e9ccda8811dbfd683b2afb
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-21 16:22:32 -0800

    Add files via upload

---
+++ randomnumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var random = new Random();
+            const int passlength = 10;
+            var buffer = new char[passlength];
+
+            for (var i = 0; i < passlength; i++)
+                // Console.Write((char)('a' + random.Next(0, 26))); 
+                buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var pass = new string(buffer);
+            Console.WriteLine("\n"+(int)'a');
+            Console.WriteLine("\n" + pass);
+
+        }
+    }
+}
