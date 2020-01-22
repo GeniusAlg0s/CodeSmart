59650ae5e47d36dcb3c89aee3f40657f0ca77138
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-22 10:31:06 -0800

    Add files via upload

--- list/Node.cs
+++ list/Node.cs
@@ -2,122 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace linkedlist
+namespace linked
 {
-  public  class Node
+    class Node
     {
-        
-        
-            public int data; //actual data stored in the node
-            public Node next;// pointer in next node
-            public Node(int d)
-            {
-            //initializing the data and pointer 
-                data = d;
-                next = null;
-            }
+        internal int data;
+        internal Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+       
+    }
+    class llist
+    {
+        public Node headOrStart;
+
+        public void isnertofront(llist a, int newData)
+        {
+            Node newNode = new Node(newData);
+            newNode.next = a.headOrStart;
+            a.headOrStart = newNode;
+        }
+
         public void print()
         {
-            Console.Write("|" + data + "|");
-            if (next != null)
+            Console.Write("|" + headOrStart + "|");
+            if (headOrStart != null)
             {
-                next.print();
+               Console.WriteLine( headOrStart.next);
             }
         }
-        public void insertsorted(int data)
-        {
-            if(next == null)
-            {
-                next = new Node(data);
-            }
-            else if (data < next.data)
-            {
-                Node temp = new Node(data);
-                temp.next = this.next;
-                this.next = temp;
-            }
-            else
-            {
-                next.insertsorted(data);
-            }
-        }
-        public void addnode(int data)
-        {
-            if(next == null)
-            {
-                next = new Node(data);
-            }
-            else
-            {
-                next.addnode(data);
-            }
-        }
-    }
 
-    public class mlist
-    {
-        public Node headnode;
 
-        public mlist()
-        {
-            headnode = null;
-        }
-        /* public void addtoend(int data)
-         {
-             if(headnode == null)
-             {
-                 headnode = new Node(data);
-             }
-             else
-             {
-                 headnode.addtoend(data);
-             }
-         }*/
-        public void addnode(int data)
-        {
-            if (headnode == null)
-            {
-                headnode = new Node(data);
-            }
-            else
-            {
-                headnode.addnode(data);
-            }
-        }
-        public void insertsorted(int data)
-        {
-            if (headnode == null)
-            {
-                headnode = new Node(data);
-            }
-            else if (data < headnode.data)
-            {
-                insertbeginnig(data);
-            }
-            else
-            {
-                headnode.insertsorted(data);
-            }
-        }
-        public void insertbeginnig(int data)
-        {
-            if (headnode == null)
-            {
-                headnode = new Node(data);
-            }
-            else
-            {
-                Node temp = new Node(data);
-                temp.next = headnode;
-                headnode = temp;
-            }
-        }
-        public void print()
-        {
-            if(headnode != null)
-            {
-                headnode.print();
-            }
-        }
     }
+    
+        
+    
 }
--- list/Program.cs
+++ list/Program.cs
@@ -1,79 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
-namespace linkedlist
+
+
+
+namespace linked
 {
     class Program
     {
-        
-            static void Main(string[] args)
+        static void Main(string[] args)
         {
+            llist q = new llist();
+            q.isnertofront(q, 2);
 
-            mlist list = new mlist();
-            list.addnode(3);
-            list.addnode(3);
-            list.addnode(3);
-            list.addnode(3);
-            Console.WriteLine();
-            list.insertbeginnig(3);
-            list.insertbeginnig(6);
-            list.insertbeginnig(9);
-            list.insertbeginnig(12);
-            Console.WriteLine();
-            list.insertsorted(3);
-            list.insertsorted(6);
-            list.insertsorted(9);
-            list.insertsorted(12);
-
-            list.print();
-            // Node fnode = new Node(11);
-            /*  fnode.next = new Node(1);
-              fnode.next.next = new Node(15);
-              fnode.next.next.next = new Node(12); 
-
-            fnode.addnode(5);
-            fnode.addnode(12);
-            fnode.addnode(13); 
-
-            fnode.print(); */
-            /*
-              LinkedList<string> tests = new LinkedList<string>();
-              tests.AddLast("hello");
-              tests.AddFirst("Hey");
-
-              var enterpoint = tests.Find("Hey");
-              tests.AddAfter(enterpoint, "guten tag");
-
-              foreach (string test in tests)
-              {
-                  Console.WriteLine(tests);
-              } 
-
-            LinkedList<string> months = new LinkedList<string>();
-            months.AddLast("March");
-            months.AddFirst("January");
-
-            var march = months.Find("March");
-
-            months.AddBefore(march, "February");
-            months.AddAfter(march, "April");
-
-            foreach (string month in months)
-            {
-                Console.WriteLine(month);
-            }
-
-            LinkedList<string> days = new LinkedList<string>();
-
-            days.AddFirst("Monday");
-            days.AddLast("Saturday");
-
-            foreach (string day in days)
-            {
-                Console.WriteLine(day);
-            }
-            */
+            q.print();
         }
     }
 }
