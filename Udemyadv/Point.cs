d2a31884c8b59bb31b91426512a0d0f25565e13f
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 10:43:36 -0800

    Add files via upload

---
+++ Udemyadv/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newlocation)
+        {
+            if(newlocation == null)
+            {
+                throw new ArgumentException("newlocation");
+            }
+                
+            Move(newlocation.X, newlocation.Y);
+          /*  this.X = newlocation.X;
+            this.Y = newlocation.Y; */
+        }
+    }
+        
+}
---
+++ Udemyadv/calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methods
+{
+    public class calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach(var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
