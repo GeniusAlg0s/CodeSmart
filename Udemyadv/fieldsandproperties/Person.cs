f512505ee20cb9743586135b131922ce51cfdbff
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 11:12:58 -0800

    Add files via upload

---
+++ Udemyadv/fieldsandproperties/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace accessModifiers_public_and_private
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthDate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+        public DateTime GetBirthDate()
+        {
+            return _birthdate;
+        }
+    }
+}
