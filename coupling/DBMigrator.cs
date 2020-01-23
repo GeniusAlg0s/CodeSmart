9dd036a77c6d68480a777562b5d2addfde5d44d1
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 12:47:14 -0800

    Add files via upload

---
+++ coupling/DBMigrator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition
+{
+    class DBMigrator
+    {
+        private readonly Logger _logger;
+        public DBMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.Log("Migrateing blah...");
+        }
+    }
+}
