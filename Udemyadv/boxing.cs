d3e88be136634a8b4875947f18ab641172abe196
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 15:04:33 -0800

    Create boxing.cs

---
+++ Udemyadv/boxing.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+
+            list.Add(1);
+            list.Add("ww");
+
+            var number = (int)list[1];
+
+            
+        }
+    }
+}
