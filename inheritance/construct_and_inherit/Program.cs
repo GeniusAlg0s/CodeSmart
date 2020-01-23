4edf9f919e00e1099cd2fae59402ad4feec54391
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 14:37:06 -0800

    Add files via upload

---
+++ inheritance/construct_and_inherit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace construct_and_inherit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("12344");
+
+        }
+    }
+}
---
+++ inheritance/construct_and_inherit/Vehicles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace construct_and_inherit
+{
+    public class Vehicle
+    {
+        public readonly string _registrationNumer;
+     /*   public Vehicle()
+            {
+            Console.WriteLine("cehicle initialized");
+            }*/
+        public Vehicle(string registrationNumer)
+        {
+            _registrationNumer = registrationNumer;
+            Console.WriteLine("car beng initilized. registration is {0}", registrationNumer);
+            //Console.WriteLine("cehicle initialized");
+        }
+    }
+
+   
+
+        
+}
