09b5a249a982b75968108a044f4605a3906deecf
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 10:56:59 -0800

    Create customer.cs

---
+++ Udemyadv/fields/customer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public  List<Order> Orders = new List<Order>();
+        //public readonly List<Order> Orders = new List<Order>();
+
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+        public Customer(int id, string name)
+            :this(id)
+        {
+            this.Name = name;
+        }
+        public void Promote()
+        {
+            Orders = new List<Order>();
+        }
+
+    }
+    
+}
