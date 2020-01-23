22915df5e5c6d0d2efe342feb4bae9d8e8390338
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 12:10:03 -0800

    Add files via upload

---
+++ inheritance/first/Presentation_Object.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class_coupling
+{
+    class Presentation_Object
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Coppied to clipboard");
+        }
+        public void Duplicate()
+        {
+            Console.WriteLine("Duplicated");
+        }
+    }
+}
---
+++ inheritance/first/Text.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class_coupling
+{
+    class Text : Presentation_Object
+    {
+        public int Fontsize { get; set; }
+        public string Fontname { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added link to " + url);
+        }
+    }
+}
