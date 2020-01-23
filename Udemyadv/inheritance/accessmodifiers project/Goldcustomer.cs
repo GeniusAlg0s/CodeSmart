5d7080bf1388654b075c9cf91a662d08c5cf175c
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 13:55:03 -0800

    Add files via upload

---
+++ Udemyadv/inheritance/accessmodifiers project/Goldcustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace accessmodifiers
+{
+    class Goldcustomer : Customer
+    {
+            public void OfferVoucher()
+        {
+           var rating = this.CalculateRating(true);
+        }
+    }
+}
