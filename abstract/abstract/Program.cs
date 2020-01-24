b29a852431a45da2933198975450c6585de51a68
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-23 19:19:03 -0800

    Add files via upload

---
+++ abstract/abstract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace abstractclasses
+{
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("draw rectangle");
+
+        }
+        
+    }
+    class Program
+     {
+    static void Main(string[] args)
+    {
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+    }
+
+    }
+}
---
+++ abstract/abstract/Shape.cs
@@ -0,0 +1,23 @@
+﻿namespace abstractclasses
+{
+    // public class Shape
+    public  abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height{ get; set; }
+
+        /* public virtual void Draw()
+             {
+
+             }*/
+        public abstract void Draw();
+        public  void Copy()
+        {
+            System.Console.WriteLine("Copy into clip board");
+        }
+        public void Select()
+        {
+            System.Console.WriteLine("Select shape");
+        }
+    }
+}
