abc0f9e7a475133c581df49f83b82bc0364cb295
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-24 16:54:53 -0800

    Update Program.cs

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,31 +8,15 @@ namespace tictactoe
     class Program
     {
       
-        // static string choice;
-       // static int player = 1;
-       // static int XorO;
+      
         
         static void Main(string[] args)
         {
             var game = new Board();
-           // bool gameOver = false;
-
+         
             game.play(game);
 
-            // game.board();
-            // Board();
-            //game.Checkwin();
-         /*  do
-            {
-                Console.WriteLine("welcome to tic tac toe \n player (X) one goes first\n player (O) two follows");
-                Console.WriteLine();
-                game.board();
-               int XorO = int.Parse(Console.ReadLine());
-
-               
-            }
-            while (gameOver == false ); 
-             */
+           
 
 
         }
